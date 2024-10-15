0e58f12805ac33f9357b0e8ce3dafd17d15d8f8a
Leo Conforti <leoconforti@ltgk.net>
2024-10-15 15:50:40 +0000

    import

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -i
+
+set -eo pipefail
+echo "🚀 Setting up fish-bowl devcontainer..."
+
+# https://github.com/devcontainers/features/pull/770
+SHELL="$(which bash)" pnpm setup
+source /home/vscode/.bashrc
+pnpm config set store-dir $PNPM_HOME/store
+
+echo "Initializing submodules"
+git submodule update --init --recursive
+
+echo "📦 Installing repo dependencies..."
+corepack install
+corepack enable
+pnpm install
+
+echo "🏗️ Building..."
+pnpm build
+
+echo "🧪 Testing..."
+pnpm test
+
+echo "✅ Devcontainer setup complete!"
+echo "🙏 Thank you for contributing to fish-bowl!"
+echo "📝 P.S Don't forget to configure your git credentials with 'git config --global user.name you' and 'git config --global user.email you@z.com'"
